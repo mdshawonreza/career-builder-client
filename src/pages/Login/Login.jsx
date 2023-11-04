415fb4bcd79baa85ec70e5f7b93cbdecc8cb8855
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-05 03:09:31 +0600

    add this navbar in this carrer builder project

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,105 @@
+
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import swal from 'sweetalert';
+
+import { AuthContext } from "../../providers/AuthProvider";
+import { FaGoogle } from "react-icons/fa";
+
+
+const Login = () => {
+    const [loginError, setLoginError] = useState('')
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { signIn, signInWithGoogle } = useContext(AuthContext)
+
+    const handleLogin = e => {
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        setLoginError('')
+        signIn(email, password)
+            .then(result => {
+                console.log(result)
+                swal({
+                    title: "Good job!",
+                    text: "User logged in successfully",
+                    icon: "success",
+                    button: "Aww yiss!",
+                });
+                navigate(location?.state ? location.state : "/")
+            })
+            .catch(error => {
+                setLoginError(error.message)
+
+            })
+    }
+    const handleSignInWithGoogle = () => {
+        signInWithGoogle()
+            .then(result => {
+                console.log(result.user)
+                swal({
+                    title: "Good job!",
+                    text: "User logged in successfully",
+                    icon: "success",
+                    button: "Aww yiss!",
+                });
+                navigate(location?.state ? location.state : "/")
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+    return (
+        <div className="bg-base-200">
+            <div className="hero min-h-[780px] md:min-h-screen ">
+                <div className="hero-content flex-col ">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login <span className="text-orange-500">now</span>!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-[350px] md:w-[500px] max-w-sm shadow-2xl bg-base-100">
+                        {
+                            loginError && <p className="text-red-600 text-center text-sm mt-4 -mb-6 ">{loginError}</p>
+                        }
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-base font-medium">Email</span>
+                                </label>
+                                <input type="email" placeholder="Email" name="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-base font-medium">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover font-medium">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary  bg-orange-600 hover:bg-orange-700 border-orange-700  hover:border-orange-700  text-white px-2 py-2 font-semibold text-xl w-full rounded-lg">Login</button>
+                            </div>
+                        </form>
+                        <p className="text-center font-medium -mt-4 mb-2">New here ? please
+                            <Link to="/register">
+                                <button className="btn btn-link">Register</button>
+                            </Link>
+                        </p>
+                        <div className="flex justify-center">
+                            <button onClick={handleSignInWithGoogle} className=" pl-6 pr-3 py-1 text-lg font-semibold border-2 border-white hover:border-orange-400  hover:border-2 rounded-md hover:text-orange-600 w-36 mb-3">
+                                <div className="flex items-center gap-2">
+                                    <FaGoogle></FaGoogle>
+                                    <p>Google</p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,116 @@
+
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert";
+import { AuthContext } from "../../providers/AuthProvider";
+
+
+const Register = () => {
+    const { createUser } = useContext(AuthContext)
+    const [registerError, setRegisterError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    const handleRegister = e => {
+
+        e.preventDefault()
+        const displayName = e.target.name.value
+        const email = e.target.email.value
+        const photo = e.target.photo.value
+        const password = e.target.password.value
+
+        setRegisterError('')
+        setSuccess('')
+
+
+        if (password.length < 6) {
+            setRegisterError('Password should be at least 6 characters!')
+            return
+        } else if (!/^(?=.*[A-Z]).*$/.test(password)) {
+            setRegisterError('Password must have at least one Uppercase Character')
+            return
+        } else if (!/^(?=.*[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]).*$/.test(password)) {
+            setRegisterError('Password must have at least one Special Character')
+            return
+        }
+
+
+        createUser(email, password, photo, displayName)
+
+            .then(result => {
+                console.log(result)
+                Swal({
+                    title: "Good job!",
+                    text: "User created successfully",
+                    icon: "success",
+                    button: "Aww yiss!",
+                });
+            })
+            .catch(error => {
+                console.error(error.message)
+                setRegisterError(error.message)
+            })
+    }
+    return (
+        <div className="bg-base-200">
+            <div className="hero min-h-[780px] md:min-h-[60vh] ">
+                <div className="hero-content flex-col ">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Register <span className="text-orange-500">now</span>!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-[350px] md:w-[500px]  shadow-2xl bg-base-100">
+                        {
+                            registerError && <p className="text-red-600 text-center text-sm mt-4 -mb-6 ">{registerError}</p>
+                        }
+
+                        {
+                            success && <p className="text-green-600 mt-4 -mb-6 text-center text-base font-medium  ">{success}</p>
+                        }
+
+                        <form onSubmit={handleRegister} className="card-body ">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-base font-medium">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="Your name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-medium text-base">Email</span>
+                                </label>
+                                <input type="email" placeholder="Email" name="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-medium text-base">Email</span>
+                                </label>
+                                <input type="text" placeholder="Photo URL" name="photo" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-medium text-base">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover font-medium">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-2">
+                                <button className="block bg-orange-600  hover:bg-orange-700 hover:border-orange-700 text-white px-2 py-2 font-semibold text-xl w-full rounded-lg">Register</button>
+                            </div>
+                        </form>
+
+                        <p className="text-center font-medium -mt-8 mb-4">Already Register ? please
+                            <Link to="/login">
+                                <button className="btn btn-link ">Login</button>
+                            </Link>
+                        </p>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
