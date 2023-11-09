4dea344027bb071725247003e7d95e3676b55c5e
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-09 19:18:03 +0600

    add this my applied jobs in my applied page

--- src/pages/AppliedJobs/AppliedJobs.jsx
+++ src/pages/AppliedJobs/AppliedJobs.jsx
@@ -21,6 +21,7 @@ const AppliedJobs = () => {
 
     return (
        <div className="max-w-[340px] md:max-w-3xl lg:max-w-4xl mx-auto">
+             <div className="text-center max-w-[200px] md:max-w-2xl mx-auto my-12"><h2 className="text-2xl md:text-4xl font-bold pb-4 border-b-2 border-orange-500">My Applied Jobs</h2></div>
          <div className="my-10 grid grid-cols-1 md:grid-cols-2 gap-8">
             {
                 appliedJobs.map(appliedJob=><AppliedJobCard key={appliedJob._id} appliedJob={appliedJob} ></AppliedJobCard>)
