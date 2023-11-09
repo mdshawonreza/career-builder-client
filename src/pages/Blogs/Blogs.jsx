a65c34181cd9805997963be3962778bc5030fa42
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-09 18:55:56 +0600

    add this final edited all jobs page

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -43,7 +43,7 @@ const Blogs = () => {
                 <div className="collapse collapse-plus bg-base-200">
                     <input type="radio" name="my-accordion-3" />
                     <div className="collapse-title text-xl font-medium">
-                    Explain your code
+                    Explanation my code
 
                     </div>
                     <div className="collapse-content">
